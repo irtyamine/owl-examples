feb9e9f74a2049e42b16e0c7b0a7ac5be1262508
Yaokun Pan <myron.yk.pan@gmail.com>
2018-03-31 23:52:29 +1100

    Finished notifier examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { OwlNotifierService } from 'owl-ng';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -8,5 +10,28 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-    title = 'app';
+
+    public message = 'Here is your notifier message.';
+
+    public action = 'Action';
+
+    public vertical: any = 'bottom';
+
+    public horizontal: any = 'center';
+
+    public life = 0;
+
+    public type: any;
+
+    constructor( private notifier: OwlNotifierService ) {
+    }
+
+    public notify(): void {
+        this.notifier.open(this.message, this.action, {
+            verticalPosition: this.vertical,
+            horizontalPosition: this.horizontal,
+            life: this.life,
+            type: this.type
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { OWL_NOTIFIER_CONFIG, OwlFormFieldModule, OwlInputModule, OwlNotifierModule, OwlSelectModule } from 'owl-ng';
+
 @NgModule({
     declarations: [
         AppComponent
@@ -13,8 +15,24 @@ import { AppComponent } from './app.component';
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
+
+        OwlNotifierModule,
+        OwlFormFieldModule,
+        OwlInputModule,
+        OwlSelectModule,
     ],
-    providers: [],
+    providers: [
+        {
+            // Global Notifier Config
+            // this config would apply to all notifiers under this module.
+            provide: OWL_NOTIFIER_CONFIG, useValue: {
+                maxStack: 1,
+                life: 0,
+                verticalPosition: 'bottom',
+                horizontalPosition: 'center',
+            }
+        }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
